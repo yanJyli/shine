447b019738eadc9b3f166de983f195fca2084623
yanJyli <85889195+yanJyli@users.noreply.github.com>
2022-01-12 21:32:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, HashRouter} from 'react-router-dom';
 
 import MainPage from './components/Home/MainPage';
 import AccountPageForm from './components/Account/AccountPageForm';
@@ -10,10 +10,12 @@ export default class App extends Component {
   render() {
     return (
       <Routes>
+      <HashRouter>
         <Route path='/' element={<MainPage />} />
         <Route path='/reg' element={<AccountPageForm />} />
         <Route path='/account' element={<AccountPage />} />
         <Route path='/collections' element={<CollectionPage />} />
+        </HashRouter>
       </Routes>
     )
   }
