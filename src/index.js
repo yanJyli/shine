32417cbc89b99a0027a35adfd39162c027147a64
yanJyli <85889195+yanJyli@users.noreply.github.com>
2022-01-13 18:49:16 +0300

    .

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/shine">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
