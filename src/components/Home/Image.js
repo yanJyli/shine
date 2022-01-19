7eb161406ff6664a48c5d6867e3130468a93fc47
yanJyli <85889195+yanJyli@users.noreply.github.com>
2022-01-19 22:43:35 +0300

    /

--- src/components/Home/Image.js
+++ src/components/Home/Image.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Image ({src, caption}) {
-    return <img src={src} alt={caption} className='w-1/4 object-cover mt-4 mr-8'/>
+    return <img src={src} alt={caption} className='w-1/4 object-cover mt-4 mr-8 hover:w-1/2 duration-500 '/>
 }
 
 Image.defaultProps = {
